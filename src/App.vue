e9d7a2f81fa134108e558df4a7650e24d04d9e7a
Niewolinsky <design.niewolinsky@gmail.com>
2021-07-09 16:24:30 +0200

    INITIAL COMMIT

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Simple Vue3 Projects</h1>
+</template>
+
+<script setup>
+import Template from "./components/Template.vue";
+</script>
+
+<style lang='sass'>
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './global.sass'
+
+createApp(App).mount('#app')
